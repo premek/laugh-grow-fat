e643e4cb078bc18a6af28f05568eb2e8efb181fb
premek <premysl.vyhnal@gmail.com>
2018-05-10 00:57:03 +0200

    touch/mouse controls, scalable screen

--- src/main.lua
+++ src/main.lua
@@ -1,3 +1,4 @@
+local conf = require('conf').t
 local res = require 'resources'
 local lg = love.graphics
 
@@ -38,35 +39,43 @@ foods={
 	{_,_,_,_,_,_,_},
 	{_,_,_,_,_,_,_},
 	{_,_,_,_,_,_,_}}
+playing = true
 end
-
-
 
 local lx = 173
 local ly = 130
 local potw = 22
 function love.draw()
+	local lgw, lgh = lg.getDimensions()
+	local sx = lgw/conf.window.width
+	local sy = lgh/conf.window.height
+	local scale = math.min(1, math.min(sx, sy))
+	lgw, lgh = lgw/scale, lgh/scale
+	lg.scale(scale, scale)
+
+	local x0 = math.floor(math.max(0, (lgw-res.img.bg:getWidth())/2))
+	local y0 = math.floor(math.max(0, (lgh-res.img.bg:getHeight())/2))
 	lg.setColor(1,1,1)	
-	lg.draw(res.img.bg, 0, 0)
+	lg.draw(res.img.bg, x0, y0)
 
 	lg.setFont(res.font.lcd)
 	lg.setColor(res.palette.on)
-	lg.printf(score, 0, 105, 335, 'right')
+	lg.printf(score, x0, y0+105, 335, 'right')
 
 	for line,v in ipairs(foods) do
 		for food, visible in ipairs(v) do
-			if visible then lg.draw(res.img.food[food], lx, ly+(line-1)*34) end
+			if visible then lg.draw(res.img.food[food], x0+lx, y0+ly+(line-1)*34) end
 		end
 	end
 	
 	for pot, visible in ipairs(pots) do
-		if visible then lg.draw(res.img.pot, lx+5+(pot-1)*potw, 310) end
+		if visible then lg.draw(res.img.pot, x0+lx+5+(pot-1)*potw, y0+310) end
 	end
 	for splosh, visible in ipairs(sploshes) do
-		if visible then lg.draw(res.img.splosh, lx+5+(splosh-1)*potw, 335) end
+		if visible then lg.draw(res.img.splosh, x0+lx+5+(splosh-1)*potw, y0+335) end
 	end
 	for cook=0, lives-1 do	
-		lg.draw(res.img.cook, lx+cook*25, 100)
+		lg.draw(res.img.cook, x0+lx+cook*25, y0+100)
 	end
 
 end
@@ -117,24 +126,34 @@ function love.update(dt)
 	end
 end
 
-function love.keypressed(key)
-	if key == 'escape' then love.event.quit() end
-	
-	if not playing and not gameover then 
-		reset()
-		playing = true
+function left()
+	if not pots[1] then
+		--res.sfx.move.src:play()
+		table.remove(pots, 1)
+		table.insert(pots, _)
+	end
+end
 
-  	elseif key == 'left' then
-		if not pots[1] then
-			--res.sfx.move.src:play()
-			table.remove(pots, 1)
-			table.insert(pots, _)
-		end
-  	elseif key == 'right' then
-		if not pots[#pots] then
-			--res.sfx.move.src:play()
-			table.remove(pots, #pots)
-			table.insert(pots, 1, _)
-		end
-  	end
+function right()
+	if not pots[#pots] then
+		--res.sfx.move.src:play()
+		table.remove(pots, #pots)
+		table.insert(pots, 1, _)
+	end
+end
+
+
+function love.keypressed(key)
+	if key == 'escape' then love.event.quit()
+	elseif not playing and not gameover then reset()
+	elseif key == 'left' then left()
+	elseif key == 'right' then right() end
+end
+
+function love.mousereleased(x, y, button)
+   if button == 1 then
+	if not playing and not gameover then reset()
+	elseif x<lg:getWidth()*0.45 then left()
+	elseif x>lg:getWidth()*0.55 then right() end
+   end
 end
